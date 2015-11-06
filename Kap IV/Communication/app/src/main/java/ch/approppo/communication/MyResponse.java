4dbb673ebdf48d2cc0afc755a0bc7fdd4df16b81
Martin Neff <martin.neff@approppo.ch>
2015-11-06 23:31:01 +0100

    Communication

---
+++ Kap IV/Communication/app/src/main/java/ch/approppo/communication/MyResponse.java
@@ -0,0 +1,17 @@
+package ch.approppo.communication;
+
+/**
+ * Created by Martin Neff @approppo GmbH on 06.11.15.
+ */
+public class MyResponse {
+
+	private String response;
+
+	public String getResponse() {
+		return response;
+	}
+
+	public void setResponse(String response) {
+		this.response = response;
+	}
+}
